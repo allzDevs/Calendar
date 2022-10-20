f06477481879aca0e37edf91a576b95b56269ff7
Luciano Zangeronimo <zangeronimo@gmail.com>
2022-10-19 19:52:13 -0700

    feat: create date utils and constants file

---
+++ src/Utils/Consts.ts
@@ -0,0 +1,3 @@
+export const DAYSOFMONTH = 42;
+export const ALLNAMEOFWEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+export const ALLNAMEOFMONTHS = ['January', 'February', 'March', 'April', 'May', 'June',' July', 'August', 'September', 'October', 'November','December']

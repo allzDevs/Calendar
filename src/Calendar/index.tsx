ebcb0ac09c50ca8163997f707e60b1d590bfc637
Luciano Zangeronimo <zangeronimo@gmail.com>
2022-10-20 21:07:07 -0700

    feat: create a hook to abstract the logic

--- src/Calendar/index.tsx
+++ src/Calendar/index.tsx
@@ -1,32 +1,23 @@
-import { View, Text } from '@/Components';
-import { ALLNAMEOFWEEK, DAYSOFMONTH } from '@/Utils/Consts';
-import { getFirstDayOfMonthInWeek, getLastDayOfMonth } from '@/Utils/Date';
+import { View, Text, Button } from '@/Components';
+import { ALLNAMEOFMONTHS, ALLNAMEOFWEEK } from '@/Utils/Consts';
+import { useCalendar } from '@/Hooks/useCalendar';
 
 import '@/Styles/Global.scss';
 import Styles from './styles.module.scss';
 
 export const Calendar = () => {
   
-  const today = new Date()
-  const currentMonth = today.getMonth()
-  const currentYear = today.getFullYear()
-  const lastDayOfMonth = getLastDayOfMonth(currentYear, currentMonth);
-  
-  const lastMonthDate = new Date(today.setMonth(currentMonth - 1))
-  const lastMonth = lastMonthDate.getMonth()
-  const lastYear = lastMonthDate.getFullYear()
-  const lastDayOfLastMonth = getLastDayOfMonth(lastYear, lastMonth);
-  
-  const firstDayOfMonthInWeek = getFirstDayOfMonthInWeek(currentYear, currentMonth)
-
-  const daysOfLastMonth = new Array(firstDayOfMonthInWeek).fill(0).map((_, index) => lastDayOfLastMonth - index).reverse()
-  const daysOfMonth = new Array(lastDayOfMonth).fill(0).map((_, index) => index + 1)
-  const daysOfNextMonth = new Array(DAYSOFMONTH -lastDayOfMonth - firstDayOfMonthInWeek).fill(0).map((_, index) => index + 1)
-
-  const fullMonth = [...daysOfLastMonth, ...daysOfMonth, ...daysOfNextMonth]
+  const { createMonth, currentMonth } = useCalendar()
+  const fullMonth = createMonth()
 
   return (
     <View className={Styles.container}>
+      <View className={Styles.header}>
+        <Button name="previous" label="<" />
+        <Text>{ALLNAMEOFMONTHS[currentMonth]}</Text>
+        <Button name="next" label=">" />
+      </View>
+
       <View className={Styles.namesOfWeek}>
         {ALLNAMEOFWEEK.map(name => (<Text key={name} className={Styles.nameOfWeek}>{name}</Text>))}
       </View>
